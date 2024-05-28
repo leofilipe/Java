614c749f612909c869a56f9be87881fc31b1613c
Leonardo Carvalho <lfilipebsc@gmail.com>
2024-05-28 14:33:07 -0300

    Front-end initial upload

--- crud-angular-spring/crud-angular/src/app/app.routes.ts
+++ crud-angular-spring/crud-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'clients', pathMatch:'full'},
+    {path: 'clients', loadChildren: () =>
+        import('./clients/clients.module').then(m => m.ClientsModule)
+    }
+];
---
+++ crud-angular-spring/crud-angular/src/app/clients/clients.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientsRoutingModule } from './clients-routing.module';
+
+import {MatTableModule} from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientsRoutingModule,
+    MatTableModule
+  ]
+})
+export class ClientsModule { }
