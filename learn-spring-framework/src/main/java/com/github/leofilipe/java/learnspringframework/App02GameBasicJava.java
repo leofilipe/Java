81ed1341798f1f630481d65669249aae1b474f9b
Leonardo Carvalho <lfilipebsc@gmail.com>
2024-06-07 21:44:12 -0300

    Project update

---
+++ learn-spring-framework/src/main/java/com/github/leofilipe/java/learnspringframework/App02GameBasicJava.java
@@ -0,0 +1,15 @@
+package com.github.leofilipe.java.learnspringframework;
+
+import com.github.leofilipe.java.learnspringframework.game.GameRunner;
+import com.github.leofilipe.java.learnspringframework.game.PacmanGame;
+
+public class App02GameBasicJava {
+
+public static void main(String[] args) {
+		
+		var game = new PacmanGame();
+		var gameRunner = new GameRunner(game);
+		gameRunner.run();
+		
+	}
+}
