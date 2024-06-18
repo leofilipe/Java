d71470b67925b294688d65422ea34dce43e0f65c
Leonardo Carvalho <lfilipebsc@gmail.com>
2024-06-18 14:49:29 -0300

    Start of spring-boot project

---
+++ learn-spring/learn-spring-boot/src/test/java/com/github/leofilipe/java/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leofilipe.java.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
