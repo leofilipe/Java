17fad1c78b59248c31b495454d8ff934bb6e9ce6
Leonardo Carvalho <lfilipebsc@gmail.com>
2024-07-03 10:36:21 -0300

    update to REST lessons

--- learn-spring/restful-web-services/src/main/java/com/github/leofilipe/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ learn-spring/restful-web-services/src/main/java/com/github/leofilipe/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.github.leofilipe.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,11 @@ public class HelloWorldController {
 	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="hello-world/path-variable/{name}")
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
 }
